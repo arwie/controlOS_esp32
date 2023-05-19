12b9f2f537bfaec6dc2ccd7f9ae54dbfc103616a
Artur Wiebe <artur@4wiebe.de>
2023-05-19 11:27:11 +0200

    handle ethernet link events

--- root/local_src/esp32/app.cpp
+++ root/local_src/esp32/app.cpp
@@ -32,41 +32,41 @@ static const char* TAG = "app";
 
 
 #ifndef APP_NET_ETH
-	static void main_wifiScan(void* arg, esp_event_base_t, int32_t, void*)
-	{
-		ESP_LOGI(TAG, "wifiScan");
-		wifi_scan_config_t scanConfig = {};
-		scanConfig.bssid = ((wifi_config_t*)arg)->sta.bssid;
-		ESP_ERROR_CHECK(esp_wifi_scan_start(&scanConfig, false));
-	}
+static void main_wifiScan(void* arg, esp_event_base_t, int32_t, void*)
+{
+	ESP_LOGI(TAG, "wifiScan");
+	wifi_scan_config_t scanConfig = {};
+	scanConfig.bssid = ((wifi_config_t*)arg)->sta.bssid;
+	ESP_ERROR_CHECK(esp_wifi_scan_start(&scanConfig, false));
+}
 
-	static void main_wifiConnect(void* arg, esp_event_base_t, int32_t, void*)
-	{
-		ESP_LOGI(TAG, "wifiConnect");
-		
-		uint16_t apMax = 1, apCount = 0; wifi_ap_record_t apInfo = {};
-		ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&apMax, &apInfo));
-		ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&apCount));
-		if (!apCount) {
-			ESP_LOGE(TAG, "wifi ap not found");
-			esp_restart();
-		}
-		wifi_config_t* wifiConfig = (wifi_config_t*) arg;
-		strncpy((char*) wifiConfig->sta.ssid, (char*) apInfo.ssid, sizeof(wifiConfig->sta.ssid));
-		ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, wifiConfig));
-		ESP_ERROR_CHECK(esp_wifi_connect());
-	}
-
-	static void main_wifiConnected(void* arg, esp_event_base_t, int32_t, void*) {
-		ESP_LOGI(TAG, "wifiConnected");
-		xSemaphoreGive(*((SemaphoreHandle_t*)arg));
-	}
-
-	static void main_wifiDisconnected(void*, esp_event_base_t, int32_t, void*) {
-		ESP_LOGE(TAG, "wifiDisconnected");
+static void main_wifiConnect(void* arg, esp_event_base_t, int32_t, void*)
+{
+	ESP_LOGI(TAG, "wifiConnect");
+	
+	uint16_t apMax = 1, apCount = 0; wifi_ap_record_t apInfo = {};
+	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&apMax, &apInfo));
+	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&apCount));
+	if (!apCount) {
+		ESP_LOGE(TAG, "wifi ap not found");
 		esp_restart();
 	}
+	wifi_config_t* wifiConfig = (wifi_config_t*) arg;
+	strncpy((char*) wifiConfig->sta.ssid, (char*) apInfo.ssid, sizeof(wifiConfig->sta.ssid));
+	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, wifiConfig));
+	ESP_ERROR_CHECK(esp_wifi_connect());
+}
 #endif
+
+static void main_networkConnected(void* arg, esp_event_base_t, int32_t, void*) {
+	ESP_LOGI(TAG, "networkConnected");
+	xSemaphoreGive(*((SemaphoreHandle_t*)arg));
+}
+
+static void main_networkDisconnected(void*, esp_event_base_t, int32_t, void*) {
+	ESP_LOGE(TAG, "networkDisconnected");
+	esp_restart();
+}
 
 
 static void main_init()
@@ -77,12 +77,13 @@ static void main_init()
 	const char *cfgData; spi_flash_mmap_handle_t cfgDataHandle;
 	ESP_ERROR_CHECK(esp_partition_mmap(cfgPart, 0, cfgPart->size, SPI_FLASH_MMAP_DATA, (const void**) &cfgData, &cfgDataHandle));
 	deserializeJson(cfg, cfgData);
-	spi_flash_munmap(cfgDataHandle);
 
 	//### networking init
 	ESP_ERROR_CHECK(esp_netif_init());
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
 	esp_netif_t	*netif = NULL;
+	StaticSemaphore_t networkConnectedSemaphoreBuffer;
+	SemaphoreHandle_t networkConnectedSemaphore = xSemaphoreCreateBinaryStatic(&networkConnectedSemaphoreBuffer);
 
 	#ifdef APP_NET_ETH
 
@@ -93,6 +94,11 @@ static void main_init()
 		esp_eth_handle_t ethHandle = NULL;
 		ESP_ERROR_CHECK(esp_eth_driver_install(&ethConfig, &ethHandle));
 		ESP_ERROR_CHECK(esp_netif_attach(netif, esp_eth_new_netif_glue(ethHandle)));
+
+		//### ethernet start
+		esp_event_handler_instance_t ethConnectedHandler, ethDisconnectedHandler;
+		ESP_ERROR_CHECK(esp_event_handler_instance_register(ETH_EVENT, ETHERNET_EVENT_CONNECTED,    &main_networkConnected, &networkConnectedSemaphore, &ethConnectedHandler));
+		ESP_ERROR_CHECK(esp_event_handler_instance_register(ETH_EVENT, ETHERNET_EVENT_DISCONNECTED, &main_networkDisconnected, NULL, &ethDisconnectedHandler));
 		ESP_ERROR_CHECK(esp_eth_start(ethHandle));
 
 	#else
@@ -111,12 +117,11 @@ static void main_init()
 			wifiConfig.sta.bssid[i] = cfgWifiBssid[i];
 		
 		//### wifi start
-		StaticSemaphore_t wifiConnectedSemaphoreBuffer; SemaphoreHandle_t wifiConnectedSemaphore = xSemaphoreCreateBinaryStatic(&wifiConnectedSemaphoreBuffer);
 		esp_event_handler_instance_t wifiScanHandler, wifiConnectHandler, wifiConnectedHandler, wifiDisconnectedHandler;
 		ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_START,        &main_wifiScan,         &wifiConfig, &wifiScanHandler));
 		ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_SCAN_DONE,        &main_wifiConnect,      &wifiConfig, &wifiConnectHandler));
-		ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED,    &main_wifiConnected,    &wifiConnectedSemaphore, &wifiConnectedHandler));
-		ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &main_wifiDisconnected, NULL, &wifiDisconnectedHandler));
+		ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED,    &main_networkConnected, &networkConnectedSemaphore, &wifiConnectedHandler));
+		ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &main_networkDisconnected, NULL, &wifiDisconnectedHandler));
 		ESP_ERROR_CHECK(esp_wifi_start());
 
 	#endif
@@ -145,17 +150,14 @@ static void main_init()
 	//### app init
 	ESP_LOGI(TAG, "app init");
 	init(cfg.as<JsonObject>());
+
+	//### wait network connected
+	if (!xSemaphoreTake(networkConnectedSemaphore, pdMS_TO_TICKS(30000))) {
+		ESP_LOGE(TAG, "wait network connected timeout");
+		esp_restart();
+	}
 	
-	#ifndef APP_NET_ETH
-		//### wait wifi connected
-		xSemaphoreTake(wifiConnectedSemaphore, portMAX_DELAY);
-		vSemaphoreDelete(wifiConnectedSemaphore);
-		ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_unregister(WIFI_EVENT, WIFI_EVENT_STA_START,     wifiScanHandler));
-		ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_unregister(WIFI_EVENT, WIFI_EVENT_SCAN_DONE,     wifiConnectHandler));
-		ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_unregister(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, wifiConnectedHandler));
-	#endif
-	
-	//### ota update
+	//### ota update connect
 	char otaBuffer[128];
 	esp_http_client_config_t otaClientConfig = {};
 	otaClientConfig.url = cfg["ota"];
@@ -163,6 +165,19 @@ static void main_init()
 	esp_ota_get_app_elf_sha256(otaBuffer, sizeof(otaBuffer));
 	ESP_ERROR_CHECK(esp_http_client_set_header(otaClient, "Hash", otaBuffer));
 	ESP_ERROR_CHECK(esp_http_client_open(otaClient, 0));
+
+	//### cleanup
+	#ifdef APP_NET_ETH
+		ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_unregister(ETH_EVENT, ETHERNET_EVENT_CONNECTED,  ethConnectedHandler));
+	#else
+		ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_unregister(WIFI_EVENT, WIFI_EVENT_STA_START,     wifiScanHandler));
+		ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_unregister(WIFI_EVENT, WIFI_EVENT_SCAN_DONE,     wifiConnectHandler));
+		ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_unregister(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, wifiConnectedHandler));
+	#endif
+	vSemaphoreDelete(networkConnectedSemaphore);
+	spi_flash_munmap(cfgDataHandle);
+
+	//### ota update finish
 	esp_http_client_fetch_headers(otaClient);
 	if (esp_http_client_get_status_code(otaClient) == HttpStatus_Ok)
 	{
